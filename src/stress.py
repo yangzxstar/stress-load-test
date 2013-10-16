5b69f06a81d3c6a15325f1c9a2b8276b438cab20
zhixian.yang <yangzxstar@gmail.com>
2013-10-16 18:05:41 +0800

    fix bug

--- src/stress.py
+++ src/stress.py
@@ -5,6 +5,7 @@ import logging.config
 import urllib2
 import threading
 import time
+import sys
 
 logging.config.dictConfig({
     "version": 1,
@@ -47,38 +48,72 @@ class StressTest(threading.Thread):
         global success
         global fail
 
-        count = 0
-
         while True:
-            count += 1
-            time.sleep(self.sleep_time)
+            p_time = time.time()
             content = urllib2.urlopen(self.url).read()
+            #log.info("current time is " + str(time.time()))
 
             if content.strip():
                 if mutex.acquire(1):
                     success += 1
                     mutex.release()
 
-                log.info(self.name + ' access ' + self.url + ' success')
+                    ##log.info(self.name + ' access ' + self.url + ' success')
 
             else:
                 if mutex.acquire(1):
                     fail += 1
                     mutex.release()
 
-                log.info(self.name + ' access ' + self.url + ' fail')
+                    ##log.info(self.name + ' access ' + self.url + ' fail')
 
-            if count % 10 == 0:
-                log.info(
-                    self.name + ' success accessed times is ' + str(success) + ' failed accessed times is ' + str(fail))
+                    ##if count % 100 == 0:
+                    ##  log.info(
+                    ## self.name + ' success accessed times is ' + str(success) + ' failed accessed times is ' + str(fail))
+
+            last_time = time.time() - p_time
+            ##log.info("request time is " + str(last_time))
+            if last_time >= self.sleep_time:
+                continue
+            else:
+                time.sleep(self.sleep_time - last_time)
+
+
+## statistic thread
+class Watcher(threading.Thread):
+    def __init__(self, time_unit):
+        threading.Thread.__init__(self)
+        self.time_unit = time_unit
+
+    def run(self):
+        global success
+        global fail
+        global start_time
+
+        while True:
+            previous_success = success
+            previous_fail = fail
+            time.sleep(self.time_unit)
+
+            total_current_req = success + fail - previous_success - previous_fail
+
+            log.info("total send " + str(success + fail) + " , it costs " + str(time.time() - start_time))
+
+            if success + fail > 0 and total_current_req > 0:
+                log.info('average qps : ' + str((success + fail) / (time.time() - start_time)) + " ration : " + str(
+                    success * 100.0 / (success + fail)) + "% current qps : " + str(
+                    (success + fail - previous_success - previous_fail) / statistic_time) + " ration : " + str(
+                    (success - previous_success) * 100.0 / total_current_req) + "%")
 
 
 success = 0
 fail = 0
 mutex = threading.Lock()
+statistic_time = 5  # 统计区间，
+start_time = time.time()
 
 
-def stress_load_work(url, qps, thread_num, lasting):
+def stress_load_work(url, qps, thread_num):
     assert (qps > 0)
     assert (thread_num > 0)
 
@@ -86,13 +121,22 @@ def stress_load_work(url, qps, thread_num, lasting):
     for i in range(thread_num):
         work = StressTest("thread" + str(i), url, qps)
         work.start()
-        threads.insert(work)
+        threads.insert(0, work)
 
-    time.sleep(lasting)
+    #run statistic thread
+    watcher_thread = Watcher(statistic_time)
+    watcher_thread.start()
 
 
 if __name__ == '__main__':
-    stress_load_work('http://exp.renren.com', 100, 2, 10)
+    if len(sys.argv) < 4:
+        print 'parameter is not correct'
+        print 'you need to input url, qps per thread, number of threads'
+
+    else:
+        print 'you are going to load stress test on ' + str(sys.argv[1])
+        print 'the qps per thread is ' + sys.argv[2] + ', and the thread num is ' + sys.argv[3]
+        stress_load_work(sys.argv[1], float(sys.argv[2]), int(sys.argv[3]))
 
 
 
